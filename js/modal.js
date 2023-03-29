9a7a7be8cca26cf18cd7174254f61aeeea4bce00
Yevhenii-Lukianenko <zhenyalukyanenko17@gmail.com>
2023-03-29 20:23:08 +0300

    adds check condition of form modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,24 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    checkboxInput: document.querySelector('.checkbox__input'),
+    sendModalBtn: document.querySelector('.modal__btn--send'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.checkboxInput.addEventListener('click', toggleDisabledSendBtn);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleDisabledSendBtn() {
+    if (refs.checkboxInput.checked) {
+      return (refs.sendModalBtn.disabled = false);
+    }
+    refs.sendModalBtn.disabled = true;
   }
 })();
